9258bf94a3f3b58f9172f83d22591faa1b22c7af
Bozana Bokan <bozana.bokan@posteo.net>
2025-08-15 10:03:48 +0200

    submissinFile

--- classes/submissionFile/Repository.php
+++ classes/submissionFile/Repository.php
@@ -23,6 +23,7 @@ use Exception;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\LazyCollection;
 use PKP\config\Config;
 use PKP\core\Core;
 use PKP\core\PKPApplication;
@@ -918,5 +919,34 @@ abstract class Repository
         }
 
         return $retValue;
+    }
+
+    /**
+     * Get all minor versions' submission files of the same submission, that are
+     * with the same version stage, version major and DOI ID
+     * as the given submission file
+     *
+     * @return LazyCollection<int,T>
+     */
+    public function getMinorVersionsWithSameDoi(SubmissionFile $submissionFile): LazyCollection
+    {
+        $publication = Repo::publication()->get($submissionFile->getData('publicationId'));
+        $allMinorVersionIds = Repo::publication()->getCollector()
+            ->filterBySubmissionIds([$publication->getData('submissionId')])
+            ->filterByVersionStage($publication->getData('versionStage'))
+            ->filterByVersionMajor($publication->getData('versionMajor'))
+            ->getIds()
+            ->values()
+            ->toArray();
+        $publicationFormatIds = DB::table('publication_formats')
+            ->whereIn('publication_id', $allMinorVersionIds)
+            ->select('publication_format_id')
+            ->pluck('publication_format_id')
+            ->toArray();
+        return $this->getCollector()
+            ->filterBySubmissionIds([$submissionFile->getData('submissionId')])
+            ->filterByAssoc(Application::ASSOC_TYPE_PUBLICATION_FORMAT, $publicationFormatIds)
+            ->filterByDoiIds([$submissionFile->getData('doiId')])
+            ->getMany();
     }
 }
