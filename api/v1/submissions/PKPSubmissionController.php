1420de72fae2e0f48f322ab47ee463046aa4005c
Bozana Bokan <bozana.bokan@posteo.net>
2026-06-09 13:55:41 +0200

    pkp/pkp-lib#7527 Stamp context identity metadata onto publications and issues

--- api/v1/submissions/PKPSubmissionController.php
+++ api/v1/submissions/PKPSubmissionController.php
@@ -729,9 +729,6 @@ class PKPSubmissionController extends PKPBaseController
         $submissionId = Repo::submission()->add($submission, $publication, $request->getContext());
 
         $submission = Repo::submission()->get($submissionId);
-        $publication = $submission->getCurrentPublication();
-        $publication->stampContextIdentity();
-        Repo::publication()->edit($publication, []);
 
         // Assign submitter to submission
         Repo::stageAssignment()
@@ -1353,6 +1350,11 @@ class PKPSubmissionController extends PKPBaseController
 
         $params = $this->convertStringsToSchema(PKPSchemaService::SCHEMA_PUBLICATION, $illuminateRequest->input());
         $params['id'] = $publication->getId();
+
+        $writeDisabledErrors = $this->getWriteDisabledErrors(PKPSchemaService::SCHEMA_PUBLICATION, $params);
+        if (!empty($writeDisabledErrors)) {
+            return response()->json($writeDisabledErrors, Response::HTTP_BAD_REQUEST);
+        }
 
         if (array_key_exists('status', $params) && is_null($params['status'])) {
             unset($params['status']);
--- classes/publication/Repository.php
+++ classes/publication/Repository.php
@@ -309,6 +309,8 @@ abstract class Repository
 
     /**
      * Perform validations that should be treated as warnings instead of errors.
+     *
+     * @hook Publication::validatePublishWarnings [[&$warnings, $publication, $submission, $allowedLocales, $primaryLocale]]
      */
     public function validatePublishWarnings(Publication $publication, Submission $submission, array $allowedLocales, string $primaryLocale): array
     {
@@ -405,6 +407,8 @@ abstract class Repository
 
         if ($context->getData(Context::SETTING_DOI_VERSIONING) && !$isMinorVersion) {
             $newPublication->setData('doiId', null);
+            // Clear identity so a new-DOI version is re-stamped on publish; others inherit.
+            $newPublication->clearIdentityMetadata();
         }
 
         $citations = $newPublication->getData('citations');
@@ -574,16 +578,17 @@ abstract class Repository
         $newPublication = clone $publication;
         $newPublication->stampModified();
 
-        if (!$newPublication->getData('datePublished')) {
-            $newPublication->stampContextIdentity();
-        }
-
         $this->setStatusOnPublish($newPublication);
 
         // Set the copyright and license information
         $submission = Repo::submission()->get($newPublication->getData('submissionId'));
 
         $itsPublished = ($newPublication->getData('status') === PKPPublication::STATUS_PUBLISHED);
+
+        // Stamp identity on first publication, only when empty so versions inherit (see version()).
+        if ($itsPublished && !$newPublication->getData('contextName')) {
+            $newPublication->stampContextIdentity();
+        }
 
         if ($itsPublished && !$newPublication->getData('copyrightHolder')) {
             $newPublication->setData(
